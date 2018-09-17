2d1112ca602efa6e86e57978c3be6ba3f5cbd106
nalbam <me@nalbam.com>
2018-09-17 10:57:50 +0900

    release version

--- package.sh
+++ package.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-curl -s https://api.github.com/rate_limit
-echo
+# curl -s https://api.github.com/rate_limit
+# echo
 
 USERNAME=${1:-opsnow-tools}
 REPONAME=${2:-valve-ctl}
@@ -10,36 +10,31 @@ rm -rf target
 mkdir -p target/dist
 mkdir -p target/charts
 
-# OS_NAME
+# uname
 OS_NAME="$(uname | awk '{print tolower($0)}')"
-
 echo "OS_NAME=${OS_NAME}"
+echo
 
-# VERSION
+# previous versions
 VERSION=$(curl -s https://api.github.com/repos/${USERNAME}/${REPONAME}/releases/latest | grep tag_name | cut -d'"' -f4 | xargs)
-echo "VERSION=${VERSION}"
 
+# release version
 if [ -z ${VERSION} ]; then
     VERSION=$(cat ./VERSION | xargs)
 else
     MAJOR=$(cat ./VERSION | xargs | cut -d'.' -f1)
     MINOR=$(cat ./VERSION | xargs | cut -d'.' -f2)
 
-    LATEST_MAJOR=$(echo "${VERSION}" | cut -d'.' -f1)
-    LATEST_MINOR=$(echo "${VERSION}" | cut -d'.' -f2)
+    LATEST_MAJOR=$(echo ${VERSION} | cut -d'.' -f1)
+    LATEST_MINOR=$(echo ${VERSION} | cut -d'.' -f2)
 
-    if [ "x${MAJOR}" != "x${LATEST_MAJOR}" ] || [ "x${MINOR}" != "x${LATEST_MINOR}" ]; then
+    if [ "${MAJOR}" != "${LATEST_MAJOR}" ] || [ "${MINOR}" != "${LATEST_MINOR}" ]; then
         VERSION=$(cat ./VERSION | xargs)
     fi
 
     # add
-    VERSION=$(echo "${VERSION}" | perl -pe 's/^(([v\d]+\.)*)(\d+)(.*)$/$1.($3+1).$4/e')
+    VERSION=$(echo ${VERSION} | perl -pe 's/^(([v\d]+\.)*)(\d+)(.*)$/$1.($3+1).$4/e')
 fi
-
-printf "${VERSION}" > target/VERSION
-
-echo "VERSION=${VERSION}"
-echo
 
 # 755
 find ./** | grep [.]sh | xargs chmod 755
@@ -66,7 +61,3 @@ echo
 
 # target/charts/
 cp -rf charts/* target/charts/
-
-# ls
-ls -al target
-ls -al target/dist
