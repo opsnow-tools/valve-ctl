ae422d37651a3da412757385a13a150eeacd93a8
nalbam <me@nalbam.com>
2018-10-12 10:08:40 +0900

    _gen_version

--- package.sh
+++ package.sh
@@ -1,44 +1,40 @@
 #!/bin/bash
 
-# curl -s https://api.github.com/rate_limit
-# echo
+OS_NAME="$(uname | awk '{print tolower($0)}')"
 
 USERNAME=${1:-opsnow-tools}
 REPONAME=${2:-valve-ctl}
 
+_gen_version() {
+    # previous versions
+    VERSION=$(curl -s https://api.github.com/repos/${USERNAME}/${REPONAME}/releases/latest | grep tag_name | cut -d'"' -f4 | xargs)
+
+    # release version
+    if [ -z ${VERSION} ]; then
+        VERSION=$(cat ./VERSION | xargs)
+    else
+        MAJOR=$(cat ./VERSION | xargs | cut -d'.' -f1)
+        MINOR=$(cat ./VERSION | xargs | cut -d'.' -f2)
+
+        LATEST_MAJOR=$(echo ${VERSION} | cut -d'.' -f1)
+        LATEST_MINOR=$(echo ${VERSION} | cut -d'.' -f2)
+
+        if [ "${MAJOR}" != "${LATEST_MAJOR}" ] || [ "${MINOR}" != "${LATEST_MINOR}" ]; then
+            VERSION=$(cat ./VERSION | xargs)
+        fi
+
+        # add
+        VERSION=$(echo ${VERSION} | perl -pe 's/^(([v\d]+\.)*)(\d+)(.*)$/$1.($3+1).$4/e')
+    fi
+
+    printf "${VERSION}" > target/VERSION
+    echo "VERSION=${VERSION}"
+}
+
+# target
 rm -rf target
 mkdir -p target/dist
 mkdir -p target/charts
-
-# uname
-OS_NAME="$(uname | awk '{print tolower($0)}')"
-echo "OS_NAME=${OS_NAME}"
-echo
-
-# previous versions
-VERSION=$(curl -s https://api.github.com/repos/${USERNAME}/${REPONAME}/releases/latest | grep tag_name | cut -d'"' -f4 | xargs)
-
-# release version
-if [ -z ${VERSION} ]; then
-    VERSION=$(cat ./VERSION | xargs)
-else
-    MAJOR=$(cat ./VERSION | xargs | cut -d'.' -f1)
-    MINOR=$(cat ./VERSION | xargs | cut -d'.' -f2)
-
-    LATEST_MAJOR=$(echo ${VERSION} | cut -d'.' -f1)
-    LATEST_MINOR=$(echo ${VERSION} | cut -d'.' -f2)
-
-    if [ "${MAJOR}" != "${LATEST_MAJOR}" ] || [ "${MINOR}" != "${LATEST_MINOR}" ]; then
-        VERSION=$(cat ./VERSION | xargs)
-    fi
-
-    # add
-    VERSION=$(echo ${VERSION} | perl -pe 's/^(([v\d]+\.)*)(\d+)(.*)$/$1.($3+1).$4/e')
-fi
-
-printf "${VERSION}" > target/VERSION
-echo "VERSION=${VERSION}"
-echo
 
 # 755
 find ./** | grep [.]sh | xargs chmod 755
@@ -52,6 +48,10 @@ cp -rf tools.sh   target/tools
 cp -rf valve.sh target/dist/valve
 
 # version
+_gen_version
+echo
+
+# replace version
 if [ "${OS_NAME}" == "linux" ]; then
     sed -i -e "s/THIS_VERSION=.*/THIS_VERSION=${VERSION}/" target/dist/valve
 elif [ "${OS_NAME}" == "darwin" ]; then
@@ -62,6 +62,7 @@ fi
 pushd draft
 tar -czf ../target/dist/draft.tar.gz *
 popd
+
 echo
 
 # target/charts/
