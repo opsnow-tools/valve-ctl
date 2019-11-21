8ec3eca530880e9d4473f10e3c5ccbf69fcce202
longlg88 <longlg88@naver.com>
2019-11-21 02:45:44 +0000

    Hot fix valve install error / Modify LIB_DIR directory direction

--- src/valve.sh
+++ src/valve.sh
@@ -6,7 +6,7 @@ export OS_NAME="$(uname | awk '{print tolower($0)}')"
 if [ "${OS_NAME}" == "darwin" ]; then
     readonly ROOT_SHELL_DIR=$(dirname "$(readlink "$0")")
 else
-    readonly ROOT_SHELL_DIR=$HOME/.local/share
+    readonly ROOT_SHELL_DIR=$HOME/.local/share/valve
 fi
 
 export readonly ROOT_SHELL_DIR
